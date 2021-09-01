7cd2fff363066a529546a15b00b219143914cef0
saeki.koizumi <saeking.1971@gmail.com>
2021-09-01 01:40:12 +0000

    add kadai-fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz value
+    if value % 3 == 0 && value % 5 == 0
+        "FizzBuzz"
+    elsif value % 3 == 0
+        "Fizz"
+    elsif value % 5 == 0
+        "Buzz"
+    else
+        value
+    end
+end
+
+num_max = 100
+
+(1..num_max).each do |number|
+    puts fizzbuzz number
+end
